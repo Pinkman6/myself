57a71c1ff4ef988f87a127b9729120a4dc0b81bc
pinkmanliu <1007545739@qq.com>
2020-10-23 19:56:50 +0800

    '主线程第一次提交'

--- src/main/java/com/atguigu/myself/Master.java
+++ src/main/java/com/atguigu/myself/Master.java
@@ -3,5 +3,6 @@ package com.atguigu.myself;
 public class Master {
     public static void main(String[] args) {
         System.out.println("这个是主分支");
+        System.out.println("主分支提交了一次");
     }
 }
